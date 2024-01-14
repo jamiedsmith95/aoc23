d3c818ae2e8bf9b3aa6195fdb3b96da144879776
jamiedsmith95 <jamie.d.smith95@gmail.com>
2024-01-14 17:15:51 +0000

    day 11, part two done by counting steps and checking if index is an empty row/col, classes need cleaning up and reordering in a logical way

--- d11q1/cosmo.py
+++ d11q1/cosmo.py
@@ -1,6 +1,5 @@
 import argparse
 
-
 class Galaxy:
     def __init__(self,x,y,id):
         self.id = id
@@ -10,24 +9,66 @@ class Galaxy:
     def __str__(self) -> str:
         return f'id: {self.id}, x: {self.x}, y: {self.y} '
         
-    def get_steps(self,other):
-        xdiff = abs(self.x - other.x)
-        ydiff = abs(self.y - other.y)
-        if self == other:
-            return 0
+    def get_steps(self,other,expanded = True,row_indexs=[],col_indexs=[],expansion_factor=2):
+        if not expanded:
+            steps = 0
+            for i in range(min(self.x,other.x)+1,max(self.x,other.x)+1):
+                if i in col_indexs:
+                    steps += expansion_factor
+                else:
+                    steps += 1
+            for i in range(min(self.y,other.y)+1,max(self.y,other.y)+1):
+                if i in row_indexs:
+                    steps += expansion_factor
+                else:
+                    steps += 1
+            return steps
+
         else:
-            return ydiff + xdiff
+
+            xdiff = abs(self.x - other.x)
+            ydiff = abs(self.y - other.y)
+            if self == other:
+                return 0
+            else:
+                return ydiff + xdiff
 
     def __eq__(self,other):
         return self.x == other.x and self.y == other.y
 
 class Galaxies:
+    expansion_factor = 2
     def __init__(self,file_name):
         self.read_file(file_name)
-        self.expansion()
+        self.get_empty_indexs()
+        # self.expansion()
         for line in self.lines:
             print(line)
         self.find_galaxies()
+
+    def get_empty_indexs(self):
+        lines = self.lines[:]
+        columns = [[j[i] for j in lines] for i in range(len(lines[0]))]
+        col_indexs = []
+        for i in range(len(columns)):
+            column = columns[i]
+            if '#' in column:
+                pass
+            else:
+                col_indexs.append(i)
+        row_indexs = []
+        for i in range(len(lines)):
+            row = lines[i]
+            if '#' in row:
+                pass
+            else:
+                row_indexs.append(i)
+        print(row_indexs)
+        print(col_indexs)
+        self.row_indexs = row_indexs
+        self.col_indexs = col_indexs
+
+        
 
 
     def read_file(self,file_name):
@@ -40,14 +81,15 @@ class Galaxies:
         self.lines = lines
 
     def expansion(self):
-        lines = self.lines[:]
+        self.expanded_lines = self.lines[:]
+        lines = self.expanded_lines
         for i in range(len(lines)-1,-1,-1):
             line = lines[i]
             if '#' in line:
                 pass
             else:
                 print(i)
-                self.lines.insert(i,line)
+                self.expanded_lines.insert(i,line)
         indexs = []
         for i in range(len(lines[0])-1,-1,-1):
             column = [lines[j][i] for j in range(len(lines[i]))]
@@ -56,19 +98,16 @@ class Galaxies:
             else:
                 indexs.append(i)
         print(indexs)
-        for i in range(len(self.lines)):
-            line = self.lines[i]
+        for i in range(len(self.expanded_lines)):
+            line = self.expanded_lines[i]
             for index in sorted(indexs,reverse=True):
                 if  index < len(line)-1:
                     line = line[:index] + '.' + line[index:]
                 else:
                     line = line + '.'
-            self.lines[i] = line
+            self.expanded_lines[i] = line
 
-            
-
-
-    def get_steps(self):
+    def get_steps(self,expanded=True):
         steps = {}
         total_steps = 0
         for i in range(len(self.galaxies.values())):
@@ -76,7 +115,7 @@ class Galaxies:
             step = []
             for j in range(len(self.galaxies.values())):
                 other = self.galaxies[j+1]
-                step.append(galaxy.get_steps(other))
+                step.append(galaxy.get_steps(other,expanded,self.row_indexs,self.col_indexs,self.expansion_factor))
             total_steps += sum(step[i:])
             steps[galaxy.id] = step
         self.steps = steps
@@ -105,9 +144,10 @@ if __name__ == "__main__":
     parser.add_argument('file_name')
     args = parser.parse_args()
     file_name = args.file_name
+    
     galaxies = Galaxies(file_name)
-    galaxies.get_steps()
-    [print(galaxies.steps[i+1]) for i in range(len(galaxies.steps))]
+    galaxies.expansion_factor = 1000000
+    galaxies.get_steps(expanded=False)
     print(galaxies.total_steps)
 
 
